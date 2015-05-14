6a0c7f7dcd3b09121148332a4910366c19a236b2
Stevennick <stevennick@gmail.com>
2015-05-14 18:11:05 +0800

    Initial project config

---
+++ Gruntfile.js
@@ -0,0 +1,213 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Configurable paths for the application
+  var appConfig = {
+    app: require('./bower.json').appPath || 'lib',
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+
+    // project settings
+    plugin: appConfig,
+
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;' +
+      ' Licensed <%= pkg.license %> */\n',
+
+    clean: {
+      files: ['dist'],
+      server: '.tmp'
+    },
+
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: 'lib/**/*.js',
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+
+    qunit: {
+      files: 'test/**/*.html'
+    },
+
+    jshint: {
+      gruntfile: {
+        options: {
+          node: true
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: ['lib/**/*.js']
+      },
+      test: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: ['test/**/*.js']
+      }
+    },
+
+    wiredep: {
+      app: {
+        src: ['<%= plugin.app %>/../*.html'],
+        ignorePath:  /\.\.\//
+      },
+      test: {
+        devDependencies: true,
+        ignorePath:  /\.\.\//,
+        src: ['<%= plugin.app %>/../*.html'],
+        fileTypes: {
+          js: {
+            block: /(([\s\t]*)\/{2}\s*?bower:\s*?(\S*))(\n|\r|.)*?(\/{2}\s*endbower)/gi,
+            detect: {
+              js: /'(.*\.js)'/gi
+            },
+            replace: {
+              js: '\'{{filePath}}\','
+            }
+          }
+        }
+      }
+    },
+
+    watch: {
+      bower: {
+        files: ['bower.json'],
+        tasks: ['wiredep']
+      },
+      js: {
+        files: ['<%= plugin.app %>/{,*/}*.js'],
+        tasks: ['newer:jshint:all'],
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        }
+      },
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      src: {
+        files: '<%= jshint.src.src %>',
+        tasks: ['jshint:src', 'qunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'qunit']
+      },
+      livereload: {
+        options: {
+          livereload: '<%= connect.options.livereload %>'
+        },
+        files: [
+          '<%= plugin.app %>/{,*/}*.js',
+          '<%= plugin.app %>/../example.html'
+        ]
+      }
+    },
+
+    connect: {
+      options: {
+        port: 9000,
+        // Change this to '0.0.0.0' to access the server from outside.
+        hostname: 'localhost',
+        livereload: 35729
+      },
+      livereload: {
+        options: {
+          open: true,
+          // middleware: function(connect) {
+          //   return [
+          //     connect.static('.tmp'),
+          //     connect().use(
+          //       '/bower_components',
+          //       connect.static('/bower_components')
+          //     ),
+          //     connect.static(appConfig.app)
+          //   ];
+          // },
+          base: '.'
+        }
+      },
+      dist: {
+        options: {
+          open: true,
+          base: '<%= plugin.dist %>'
+        }
+      }
+    }
+
+  });
+
+  // grunt.loadNpmTasks('grunt-contrib-clean');
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-qunit');
+  // grunt.loadNpmTasks('grunt-contrib-jshint');
+  // grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  grunt.registerTask('serve', 'Compile then start a connect web server', function(target) {
+    if(target === 'dist') {
+      return grunt.task.run(['build', 'connect:dist:keepalive']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'wiredep',
+      // 'concurrent:server',
+      // 'autoprefixer:server',
+      'connect:livereload',
+      'watch'
+    ]);
+
+  });
+
+  grunt.registerTask('test', [
+    'clean',
+    'wiredep',
+    'jshint',
+    'qunit'
+  ]);
+
+  grunt.registerTask('build', [
+    'clean',
+    'wiredep',
+    'concat',
+    'uglify'
+  ]);
+
+  grunt.registerTask('default', [
+    'newer:jshint',
+    'test',
+    'build'
+  ]);
+};
